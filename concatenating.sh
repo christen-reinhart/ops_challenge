9b629ea2ff7db9d8d73011f0045ebedf8febac4f
christen <christen.reinhart@yahoo.com>
2023-10-26 20:02:27 -0400

    concatenating

---
+++ concatenating.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script Name:                  Basic concatenating.sh
+# Author:                       Christen Reinhart
+# Date of latest revision:      10/26/2023
+# Purpose:                      Purpose Demo concatenating
+# Execution                     bash concatenating.sh 
+
+# Declaration of concatenating
+
+VAR1="Hello,"
+VAR2=" World"
+VAR3="$VAR1$VAR2"
+echo "$VAR3"
