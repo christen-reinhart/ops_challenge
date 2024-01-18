6021941d289c864350cedc63163b882a4c74ce2c
christen.reinhart@yahoo.com <christen.reinhart@yahoo.com>
2024-01-17 21:26:43 -0500

    Challenge6_3

--- ops-401/401Challenge6_3.py
+++ ops-401/401Challenge6_3.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+
+# Script name Challenge: 401 Class 6
+# Author Name Christen Reinhart
+# Date of Latest Revision 01/17/2024
+# Sources
+# Purpose In Python, Create a Script 
+
+# Prompt the user to select a mode
+# Encrypt a file
+# Decrypt a file 
+# Encrypt a message
+# Decrypt a message
+
 from cryptography.fernet import Fernet
 import os
 
