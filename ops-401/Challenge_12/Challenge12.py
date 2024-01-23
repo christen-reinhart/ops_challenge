7167df2daf8dbb15c111a81ecbb5cdb0e1c94f9a
christen.reinhart@yahoo.com <christen.reinhart@yahoo.com>
2024-01-23 18:55:33 -0500

    Career12_1

--- ops-401/Challenge_12/Challenge12.py
+++ ops-401/Challenge_12/Challenge12.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python3
 
-# Script name Challenge: 401 Challenge 11
+# Script name Challenge: 401 Challenge 12
 # Author Name Christen Reinhart
-# Date of Latest Revision 01/22/2024
-# Sources https://chat.openai.com/share/ee10763b-db45-4241-a631-97da167ec5a9 
-# Purpose In Python, Create a TCP Port Range Scanner  
+# Date of Latest Revision 01/23/2024
+# Sources 
+# Purpose In Python, Generating a Range of IP Addresses from a CIDR Address in Python 
+
+#!/usr/bin/env python3
+
+import ipaddress
+from scapy.all import ICMP, IP, sr1
+
+# Define end host and TCP port range. Take care not to populate the host bits here.
+network = "10.0.2.0/24"
+ip_list = ipaddress.IPv4Network(network).hosts()
+hosts_count = 0
+
+for host in ip_list:
+    print("Pinging", str(host), "- please wait...")
+    response = sr1(
+        IP(dst=str(host))/ICMP(),
+        timeout=2,
+        verbose=0
+    )
+
+    if response:
+        print("Response received from", str(host))
+        hosts_count += 1
+    else:
+        print("No response from", str(host))
+
+print(f"\n{hosts_count} hosts responded out of {len(ip_list)}")
