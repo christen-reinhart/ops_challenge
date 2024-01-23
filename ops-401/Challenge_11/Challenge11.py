f6df187d1a584f3287015d13222db53a7089bc73
christen.reinhart@yahoo.com <christen.reinhart@yahoo.com>
2024-01-22 20:50:13 -0500

    Challenge11_DIR

---
+++ ops-401/Challenge_11/Challenge11.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Script name Challenge: 401 Class 7
+# Author Name Christen Reinhart
+# Date of Latest Revision 01/21/2024
+# Sources https://chat.openai.com/share/8a67a8cf-3f2c-4830-a3af-84b6d241ea08
+# Purpose In Python, Create a Script 
