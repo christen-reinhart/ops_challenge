3f9941aee1b1d9aedc19f43722b3d63df3256c34
christen.reinhart@yahoo.com <christen.reinhart@yahoo.com>
2024-01-23 18:17:28 -0500

    Career12_1

---
+++ ops-401/Challenge_12/Challenge12_1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Script name Challenge: 401 Challenge 12
+# Author Name Christen Reinhart
+# Date of Latest Revision 01/23/2024
+# Sources 
+# Purpose In Python, Generating a Range of IP Addresses from a CIDR Address in Python 
