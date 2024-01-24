783252b754e37937165352380c1b7dd01a1a214b
christen.reinhart@yahoo.com <christen.reinhart@yahoo.com>
2024-01-24 11:24:13 -0500

    Challenge11_4

--- ops-401/Challenge_11/Challenge11_4.py
+++ ops-401/Challenge_11/Challenge11_4.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+
+# Script name Challenge: 401 Challenge 11
+# Author Name Christen Reinhart
+# Date of Latest Revision 01/22/2024
+# Sources https://chat.openai.com/share/ee10763b-db45-4241-a631-97da167ec5a9 
+# Purpose In Python, Create a TCP Port Range Scanner 
+
 import sys
 import random
 from scapy.all import Ether, IP, sniff, ARP, sr, ICMP, TCP, send
