12d4fc85f48736fdcbb861b38c2d2d218afc7963
christen.reinhart@yahoo.com <christen.reinhart@yahoo.com>
2024-01-24 11:23:23 -0500

    Challenge11_4

---
+++ ops-401/Challenge_11/Challenge11_4.py
@@ -0,0 +1,25 @@
+import sys
+import random
+from scapy.all import Ether, IP, sniff, ARP, sr, ICMP, TCP, send
+
+# assign IP and port range
+host = 'scanme.nmap.org'
+port_range = [20, 21, 22, 23, 53, 80, 443]
+
+# function to scan ports
+def scan_ports(dst_port):
+    src_port = random.randint(1024, 65535)
+    response = sr(IP(dst=host)/TCP(sport=src_port, dport=dst_port, flags='S'), timeout=1, verbose=0)[0]
+    # conditions to perform task based on port
+    if response is None:
+        print(f"Port {dst_port} is filtered and silently dropped")
+    elif response.haslayer(TCP):
+        if response.getlayer(TCP).flags == 0x12:
+            send(IP(dst=host)/TCP(sport=src_port, dport=dst_port, flags='R'), verbose=0)
+            print(f"Port {dst_port} is open")
+        elif response.getlayer(TCP).flags == 0x14:
+            print(f"Port {dst_port} is closed")
+
+# create loop to scan ports
+for port in port_range:
+    scan_ports(port)
