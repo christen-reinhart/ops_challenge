61ceddcf7d5c843b2da37fac179fb3548af11404
christen.reinhart@yahoo.com <christen.reinhart@yahoo.com>
2024-01-24 09:25:51 -0500

    Challenge12_8

--- ops-401/Challenge_12/Challenge12_6.py
+++ ops-401/Challenge_12/Challenge12_6.py
@@ -1,4 +1,11 @@
-import ipaddress
+#!/usr/bin/env python3
+
+# Script name Challenge: 401 Challenge 12
+# Author Name Christen Reinhart
+# Date of Latest Revision 01/23/2024
+# Sources https://chat.openai.com/share/ee10763b-db45-4241-a631-97da167ec5a9
+# Purpose In Python, Generating a Range of IP Addresses from a CIDR Address in Python import ipaddress
+
 from scapy.all import *
 
 def tcp_port_range_scanner(ip, port_range):
