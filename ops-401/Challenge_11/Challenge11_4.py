bd827ed57c9983c460acbc6149bad38d481b834c
christen.reinhart@yahoo.com <christen.reinhart@yahoo.com>
2024-01-24 11:28:37 -0500

    Challenge11_4

--- ops-401/Challenge_11/Challenge11_4.py
+++ ops-401/Challenge_11/Challenge11_4.py
@@ -18,16 +18,20 @@ port_range = [20, 21, 22, 23, 53, 80, 443]
 def scan_ports(dst_port):
     src_port = random.randint(1024, 65535)
     response = sr(IP(dst=host)/TCP(sport=src_port, dport=dst_port, flags='S'), timeout=1, verbose=0)[0]
+    
     # conditions to perform task based on port
     if response is None:
         print(f"Port {dst_port} is filtered and silently dropped")
-    elif response.haslayer(TCP):
-        if response.getlayer(TCP).flags == 0x12:
-            send(IP(dst=host)/TCP(sport=src_port, dport=dst_port, flags='R'), verbose=0)
-            print(f"Port {dst_port} is open")
-        elif response.getlayer(TCP).flags == 0x14:
-            print(f"Port {dst_port} is closed")
+    else:
+        for packet in response:
+            if packet.haslayer(TCP):
+                if packet.getlayer(TCP).flags == 0x12:
+                    send(IP(dst=host)/TCP(sport=src_port, dport=dst_port, flags='R'), verbose=0)
+                    print(f"Port {dst_port} is open")
+                elif packet.getlayer(TCP).flags == 0x14:
+                    print(f"Port {dst_port} is closed")
 
 # create loop to scan ports
 for port in port_range:
     scan_ports(port)
+
