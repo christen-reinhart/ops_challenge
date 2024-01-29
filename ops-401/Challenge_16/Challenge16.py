f2c02bb4daa3ea83e21c35e37c3d6b54e0f3e261
christen.reinhart@yahoo.com <christen.reinhart@yahoo.com>
2024-01-29 12:30:50 -0500

    Challenge16

--- ops-401/Challenge_16/Challenge16.py
+++ ops-401/Challenge_16/Challenge16.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-# Script name Challenge: 401 Challenge 12
+# Script name Challenge: 401 Challenge 16
 # Author Name Christen Reinhart
-# Date of Latest Revision 01/23/2024
+# Date of Latest Revision 01/29/2024
 # Sources https://chat.openai.com/share/ee10763b-db45-4241-a631-97da167ec5a9
-# Purpose In Python, Generating a Range of IP Addresses from a CIDR Address in Python 3
+# Purpose In Python, Generating Automated Brute Force Wordlist Attack
