846afa39cda5aeed69e8715e992b46298fecc93a
christen.reinhart@yahoo.com <christen.reinhart@yahoo.com>
2024-01-29 18:19:12 -0500

    Challenge16

--- ops-401/Challenge_16/Challenge16.py
+++ ops-401/Challenge_16/Challenge16.py
@@ -4,4 +4,23 @@
 # Author Name Christen Reinhart
 # Date of Latest Revision 01/29/2024
 # Sources https://chat.openai.com/share/ee10763b-db45-4241-a631-97da167ec5a9
+# Sources https://www.geeksforgeeks.org/iterate-over-a-set-in-python/
 # Purpose In Python, Generating Automated Brute Force Wordlist Attack
+
+import ssl
+import nltk
+
+def get_words():
+    nltk.download('words')
+
+if __name__ == "__main__":
+    get_words()
+
+    
+
+    
+
+
+
+    
+    
