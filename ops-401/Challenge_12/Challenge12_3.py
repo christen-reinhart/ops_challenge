a63c69255def33fd8d15e46c536abeb41f98cbf0
christen.reinhart@yahoo.com <christen.reinhart@yahoo.com>
2024-01-29 12:14:50 -0500

    Challenge1

--- ops-401/Challenge_12/Challenge12_3.py
+++ ops-401/Challenge_12/Challenge12_3.py
@@ -4,7 +4,7 @@
 # Author Name Christen Reinhart
 # Date of Latest Revision 01/23/2024
 # Sources https://chat.openai.com/share/ee10763b-db45-4241-a631-97da167ec5a9
-# Purpose In Python, Generating a Range of IP Addresses from a CIDR Address in Python 
+# Purpose In Python, Generating a Range of IP Addresses from a CIDR Address in Python 3
 
 import sys
 from scapy.all import sr1, IP, ICMP, srp, Ether
