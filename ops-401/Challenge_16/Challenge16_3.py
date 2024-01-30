8dcefb053d06a4e080bc5856687d6ff2645b6dda
christen-reinhart <christen.reinhart@yahoo.com>
2024-01-29 22:59:01 -0500

    Challenge16_3

--- ops-401/Challenge_16/Challenge16_3.py
+++ ops-401/Challenge_16/Challenge16_3.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+
+# Script name: Challenge: 401 Challenge 16
+# Author: Christen Reinhart
+# Date of Latest Revision: 01/29/2024
+# Sources:
+#   - https://chat.openai.com/share/ee10763b-db45-4241-a631-97da167ec5a9
+#   - https://www.geeksforgeeks.org/iterate-over-a-set-in-python/
+# Purpose: In Python, Generating Automated Brute Force Wordlist Attack
+
 import nltk
 import time
 
