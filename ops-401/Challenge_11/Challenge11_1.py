9926e931c9b3eb49cf719af72ea8027ebdc40b29
christen-reinhart <christen.reinhart@yahoo.com>
2024-01-29 20:58:45 -0500

    Test

--- ops-401/Challenge_11/Challenge11_1.py
+++ ops-401/Challenge_11/Challenge11_1.py
@@ -5,7 +5,7 @@
 # Date of Latest Revision 01/22/2024
 # Sources https://chat.openai.com/share/ee10763b-db45-4241-a631-97da167ec5a9 
 # Purpose In Python, Create a TCP Port Range Scanner  
-
+# vvvvvvvvvvvvvvv
 
 from scapy.layers.inet import IP, TCP, RandShort
 from scapy.sendrecv import sr1, send
