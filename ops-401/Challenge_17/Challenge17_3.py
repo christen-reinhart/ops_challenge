fbf85b7221f7089014a0e7f1b09572587b4d5407
christen-reinhart <christen.reinhart@yahoo.com>
2024-01-31 12:00:11 -0500

    Challenge17_3

--- ops-401/Challenge_17/Challenge17_3.py
+++ ops-401/Challenge_17/Challenge17_3.py
@@ -3,7 +3,7 @@
 # Script name: Challenge: 401 Challenge 17
 # Author: Christen Reinhart
 # Date of Latest Revision: 01/30/2024
-# Sources: https://chat.openai.com/share/10b196e4-6aa3-4072-b3be-8d3d7cddc1a9
+# Sources: https://chat.openai.com/share/10b196e4-6aa3-4072-b3be-8d3d7cddc1a9 Rogers Demo
 # Purpose: In Python, Python brute force tool with the capability to dump user credential hashes
 
 import ssl
