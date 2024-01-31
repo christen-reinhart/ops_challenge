286a31dc5ae5894d4164de629bb38ff862ad8533
christen-reinhart <christen.reinhart@yahoo.com>
2024-01-30 19:30:39 -0500

    Challenge17

--- ops-401/Challenge_17/Challenge17.py
+++ ops-401/Challenge_17/Challenge17.py
@@ -4,4 +4,4 @@
 # Author: Christen Reinhart
 # Date of Latest Revision: 01/30/2024
 # Sources: https://chat.openai.com/share/10b196e4-6aa3-4072-b3be-8d3d7cddc1a9
-# Purpose: In Python, Generating Automated Brute Force Wordlist Attack
+# Purpose: In Python, Python brute force tool the capability to Dump the user credential hashes
