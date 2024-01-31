b49cb52d6caabb2234e93f5c00cc48872b218a0f
christen-reinhart <christen.reinhart@yahoo.com>
2024-01-31 17:52:43 -0500

    Challenge18

---
+++ ops-401/Challenge_18/Challenge18.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Script name: Challenge: 401 Challenge 17
+# Author: Christen Reinhart
+# Date of Latest Revision: 01/30/2024
+# Sources: https://chat.openai.com/share/10b196e4-6aa3-4072-b3be-8d3d7cddc1a9
+# Purpose: In Python, Python brute force tool with the capability to dump user credential hashes
+
+import ssl
+import nltk
+import time
+import paramiko  # need pip install
+import getpass
