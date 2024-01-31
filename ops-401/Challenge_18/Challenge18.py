c1171cfc230c9383f4305b7f5e2c38ca68cc7238
christen-reinhart <christen.reinhart@yahoo.com>
2024-01-31 18:19:38 -0500

    Challenge18_3

--- ops-401/Challenge_18/Challenge18.py
+++ ops-401/Challenge_18/Challenge18.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 
-# Script name: Challenge: 401 Challenge 18
-# Author: Christen Reinhart
-# Date of Latest Revision: 01/31/2024
-# Sources: https://chat.openai.com/share/10b196e4-6aa3-4072-b3be-8d3d7cddc1a9
-# Purpose: In Python, Finish developing a custom tool that performs brute force attacks.
-
 import ssl
 import nltk
 import time
