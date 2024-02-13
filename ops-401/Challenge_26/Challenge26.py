da9bb0d0ed21eb19b6e5ef49585f7a9de4ae925c
christen-reinhart <christen.reinhart@yahoo.com>
2024-02-12 23:11:25 -0500

    Challenge26_2

--- ops-401/Challenge_26/Challenge26.py
+++ ops-401/Challenge_26/Challenge26.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-# Script name: Challenge: 401 Challenge 18
+# Script name: Challenge: 401 Challenge 26
 # Author: Christen Reinhart
-# Date of Latest Revision: 01/31/2024
-# Sources: https://chat.openai.com/share/10b196e4-6aa3-4072-b3be-8d3d7cddc1a9
-# Purpose: In Python, Finish developing a custom tool that performs brute force attacks.
+# Date of Latest Revision: 02/12/2024
+# Sources: 
+# Purpose: In Python, Logging capabilities to your Python tool.
 
 # import libraries
 import logging
