5ac39ce88c10dabc2e394efad072dfe8ca1e3149
christen-reinhart <christen.reinhart@yahoo.com>
2024-02-12 23:04:01 -0500

    Challenge26

--- ops-401/Challenge_26/Challenge26.py
+++ ops-401/Challenge_26/Challenge26.py
@@ -5,3 +5,12 @@
 # Date of Latest Revision: 01/31/2024
 # Sources: https://chat.openai.com/share/10b196e4-6aa3-4072-b3be-8d3d7cddc1a9
 # Purpose: In Python, Finish developing a custom tool that performs brute force attacks.
+
+# import libraries
+import logging
+
+# create object log
+log = logging.getLogger("my_logger")
+
+# configure logging object
+logging.basicConfig(filename='bruteforce.log',level=logging.INFO, format='%(asctime)s - %(levelname)s -%(messages)s')
