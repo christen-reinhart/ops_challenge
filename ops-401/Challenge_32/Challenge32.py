e8687f9dfa86a63ac865dd27182d131625ca011d
christen-reinhart <christen.reinhart@yahoo.com>
2024-02-20 12:43:29 -0500

    Challenge32

--- ops-401/Challenge_32/Challenge32.py
+++ ops-401/Challenge_32/Challenge32.py
@@ -4,4 +4,40 @@
 # Author: Christen Reinhart
 # Date of Latest Revision: 02/19/2024
 # Sources: import logging
-# Purpose: In Python, Script that will perform directory search,  file name search  
+# Purpose: In Python, Generates hashes of files using hashlib 
+
+# Import libraries
+import hashlib
+from sys import platform
+import os, time
+
+# Declare functions
+
+# Function takes filename and produce hash
+def hash_file(filename):
+    
+    # Create hash object
+    h =  hashlib.sha1()
+    
+    # Open file for reading in binary mode
+    with open(filename, 'rb') as file:
+        
+        # Loop until end of file
+        chunk = 0
+        while chunk != b'':
+            # read 1024 bytes at a time
+            chunk = file.read(1024)
+            print(chunk)
+            h.update(chunk)
+            
+        # Return hex rep of hash
+        return h.hexdigest()
+    
+  # substitute file name for function
+message = hash_file("file.log")
+print(message)
+  
+    
+            
+            
+            
