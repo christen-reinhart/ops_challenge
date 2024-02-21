f91c96082cf0d524c68ab3d27b868cc07d55284a
christen-reinhart <christen.reinhart@yahoo.com>
2024-02-21 18:11:59 -0500

    Challenge33

--- ops-401/Challenge_33/Challenge33.py
+++ ops-401/Challenge_33/Challenge33.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
-# Script name: Challenge: 401 Challenge 32
+# Script name: Challenge: 401 Challenge 33
 # Author: Christen Reinhart
-# Date of Latest Revision: 02/20/2024
+# Date of Latest Revision: 02/21/2024
 # Sources: https://chat.openai.com/share/07036ee5-b77a-47d2-8da4-5d30a9a43901
-# Purpose: In Python, Generates hashes of files using hashlib
+# Purpose: In Python, generating a hash value derived from a target file, uniquely identify file using virustotal
 
-# The below demo script works in tandem with virustotal-search.py from https://github.com/eduardxyz/virustotal-search, which must be in the same directory.
-# Set your environment variable first to keep it out of your script here.
 
 import os
 
